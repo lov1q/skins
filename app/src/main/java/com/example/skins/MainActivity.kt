77290d5d17a30237a85f441ec5db5762f02e6ba4
lov1q <n.st1mer@mail.ru>
2024-11-14 11:42:46 +0300

    Initial commit

---
+++ app/src/main/java/com/example/skins/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.example.skins
+
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.main_menu, menu)
+        return true
+    }
+}
